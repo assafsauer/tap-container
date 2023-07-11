52eb1a095876bf173297346364006a9eaa41819d
assafsauer <as.sauer@gmail.com>
2023-07-11 10:28:43 +0200

    Update offline-imgpkg.sh

--- image/offline-imgpkg.sh
+++ image/offline-imgpkg.sh
@@ -3,12 +3,15 @@
 docker login -u ${INSTALL_REGISTRY_USERNAME} -p ${INSTALL_REGISTRY_PASSWORD} ${INSTALL_REGISTRY_HOSTNAME}
 docker login -u ${HARBOR_USER} -p ${HARBOR_PWD} ${HARBOR_DOMAIN}
 
-imgpkg copy \
+
+### already dockerized
+#imgpkg copy \
     -b registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:a119cb90111379a5f91d27ae572a0de860dd7322179ab856fb32c45be95d78f5 \
     --to-tar cluster-essentials-bundle-1.4.2.tar \
     --include-non-distributable-layers
 
- imgpkg copy     --tar cluster-essentials-bundle-1.4.2.tar     --to-repo $HARBOR_DOMAIN/tap/cluster-essentials-bundle     --include-non-distributable-layers    
+### copy TAP eseential package to local registry
+imgpkg copy     --tar cluster-essentials-bundle-1.4.2.tar     --to-repo $HARBOR_DOMAIN/tap/cluster-essentials-bundle     --include-non-distributable-layers    
 
  
 ### tap ###
@@ -24,11 +27,13 @@ export IMGPKG_REGISTRY_PASSWORD_1=$HARBOR_PWD
 export TAP_VERSION=$tap_release
 #export REGISTRY_CA_PATH=PATH-TO-CA
 
-imgpkg copy \
+### already dockerized
+#imgpkg copy \
   -b registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:$TAP_VERSION \
   --to-tar tap-packages-$TAP_VERSION.tar \
   --include-non-distributable-layers
 
+### copy TAP packages to local registry
   imgpkg copy \
   --tar tap-packages-$TAP_VERSION.tar \
   --to-repo $IMGPKG_REGISTRY_HOSTNAME_1/tap/tap-packages \
